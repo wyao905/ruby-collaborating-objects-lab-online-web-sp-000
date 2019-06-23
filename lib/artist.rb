941a0f58080672b7c51318ae65f41bc763223f88
Warren Yao <warrenyca905@gmail.com>
2019-06-23 06:33:52 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,21 +1,39 @@
+<<<<<<< HEAD
 class Artist
   attr_accessor :name
   
+=======
+require 'pry'
+
+class Artist
+  attr_accessor :name
+  
+  @songs = []
+>>>>>>> c0bc3748b0f79e208920bac0f2b312c5c1ed599b
   @@all = []
   
   def initialize(name)
     @name = name
+<<<<<<< HEAD
     @songs = []
   end
   
   def add_song(song)
     songs << song
+=======
+  end
+  
+  def add_song(song)
+    binding.pry
+    @songs << song
+>>>>>>> c0bc3748b0f79e208920bac0f2b312c5c1ed599b
   end
   
   def songs
     @songs
   end
   
+<<<<<<< HEAD
   def save
     @@all << self
   end
@@ -24,6 +42,8 @@ class Artist
     songs.each {|song| puts "#{song.name}"}
   end
   
+=======
+>>>>>>> c0bc3748b0f79e208920bac0f2b312c5c1ed599b
   def self.find_or_create_by_name(artist_name)
     all.each {|artist| return artist if artist.name == artist_name}
     new_artist = self.new(artist_name)
